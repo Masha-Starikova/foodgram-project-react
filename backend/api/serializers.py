054cbef3ecfcb90f8e6714c380219b31dd063315
Мария Старикова <starikova.15@yandex.ru>
2023-02-04 15:10:42 +0500

    one

---
+++ backend/api/serializers.py
@@ -0,0 +1,118 @@
+from django.contrib.auth import get_user_model
+#from drf_extra_fields.fields import Base64ImageField
+from rest_framework import serializers
+from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
+                            ShoppingCart, Tag)
+
+User = get_user_model()
+
+class TagSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для тегов
+    """
+    class Meta:
+        model = Tag
+        fields = '__all__'
+        read_only_fields = '__all__',
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для ингредиентов
+    """
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+        read_only_fields = '__all__',
+
+
+class IngredientAmountSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для вывода количества ингредиентов
+    """
+    id = serializers.ReadOnlyField(source='ingredient.id')
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit'
+    )
+
+    class Meta:
+        model = IngredientAmount
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+
+
+class AddIngredientSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для добавления Ингредиентов
+    """
+    id = serializers.PrimaryKeyRelatedField(queryset=Ingredient.objects.all())
+    amount = serializers.IntegerField()
+
+    class Meta:
+        model = IngredientAmount
+        fields = ('id', 'amount')
+
+
+class RecipeListSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для отображения рецептов
+    """
+    tags = TagSerializer(many=True, read_only=True)
+#   author = CustomUserSerializer(read_only=True)
+    ingredients = serializers.SerializerMethodField(read_only=True)
+    is_favorited = serializers.SerializerMethodField(read_only=True)
+    is_in_shopping_cart = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для добавления рецептов
+    """
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(), many=True)
+    ingredients = AddIngredientSerializer(many=True)
+#    author = CustomUserSerializer(read_only=True)
+#    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'author', 'ingredients', 'tags', 'image',
+            'name', 'text', 'cooking_time')
+
+
+class ShortRecipeSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для краткого отображения сведений о рецепте
+    """
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'image', 'cooking_time')
+
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для списка избранного
+    """
+    class Meta:
+        model = Favorite
+        fields = ('user', 'recipe')
+
+
+class ShoppingCartSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для списка покупок
+    """
+    class Meta:
+        model = ShoppingCart
+        fields = ('user', 'recipe')
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        context = {'request': request}
+        return ShortRecipeSerializer(
+            instance.recipe, context=context).data
---
+++ backend/api/views.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
+                            ShoppingCart, Tag)
+from .filters import IngredientSearchFilter, RecipeFilter
+from .pagination import CustomPageNumberPagination
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (FavoriteSerializer, IngredientSerializer,
+                          RecipeListSerializer, RecipeSerializer,
+                          ShoppingCartSerializer, TagSerializer)
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
+
+class TagsViewSet(ReadOnlyModelViewSet):
+    """
+    ViewSet для работы с тегами.
+    Добавить тег может администратор.
+    """
+    queryset = Tag.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = TagSerializer
+
+
+class IngredientsViewSet(ReadOnlyModelViewSet):
+    """
+    ViewSet для работы с ингредиентами.
+    Добавить ингредиент может администратор.
+    """
+    queryset = Ingredient.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = IngredientSerializer
+    filter_backends = [IngredientSearchFilter]
+    search_fields = ('^name',)
+
+
+class RecipeViewSet(ModelViewSet):
+    """
+    ViewSet для работы с рецептами.
+    Для анонимов разрешен только просмотр рецептов.
+    """
+    queryset = Recipe.objects.all()
+    permission_classes = [IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = RecipeFilter
+    pagination_class = CustomPageNumberPagination
+
+
