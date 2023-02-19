1ba2987ab2dcd1c15094999f774de97a31fd9ab0
Мария Старикова <starikova.15@yandex.ru>
2023-02-19 19:21:08 +0500

    test12

---
+++ backend/recipes/management/commands/load_ingredients.py
@@ -0,0 +1,21 @@
+from csv import DictReader
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    """
+    Добавляем ингредиенты из файла CSV
+    """
+
+    def handle(self, *args, **options):
+        for row in DictReader(
+            open(
+                './data/ingredients.csv'
+            ), delimiter=","):
+            ingredient=Ingredient(
+                name=row['name'], measurement_unit=row['measurement_unit']
+            )
+            ingredient.save()
