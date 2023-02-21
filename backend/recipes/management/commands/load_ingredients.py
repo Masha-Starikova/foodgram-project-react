5e00525516c7c4ff2aa8facb3597027ab4243480
Мария Старикова <starikova.15@yandex.ru>
2023-02-21 13:50:56 +0500

    recipes:v3

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for row in DictReader(open('./data/ingredients.csv'), delimiter=","):
-            ingredient=Ingredient(
+            ingredient = Ingredient(
                 name=row['name'], measurement_unit=row['measurement_unit']
             )
             ingredient.save()
