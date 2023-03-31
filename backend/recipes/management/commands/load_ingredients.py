9323c7cfe1b2b79b0acc06acb0213459a5934a37
Александр Белей <beleysasha@gmail.com>
2023-04-01 00:21:10 +1200

    color picker added (for selecting color of  tags in admin panel)

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -16,3 +16,13 @@ class Command(BaseCommand):
                 name=row['name'], measurement_unit=row['measurement_unit']
             )
             ingredient.save()
+
+
+    def handle(self, *args, **options):
+        with open('./data/ingredients.csv', newline='') as csvfile:
+            reader = DictReader(csvfile)
+            for row in reader:
+                ingredient = Ingredient(
+                    name=row['name'], measurement_unit=row['measurement_unit']
+                )
+                ingredient.save() 
