f1531f92f7808b2a1ffd0cc0d51b3dc24ca56911
Мария Старикова <starikova.15@yandex.ru>
2023-04-09 10:18:28 +0500

    ttestt4.23

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,4 +1,4 @@
-from django_filters.rest_framework import(FilterSet, filters,
+from django_filters.rest_framework import (FilterSet, filters,
                                           AllValuesMultipleFilter)
 from rest_framework.filters import SearchFilter
 
